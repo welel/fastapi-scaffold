3442e10f399d9110b4ed0ba89f50922117893f52
Pavel Loginov <56807054+welel@users.noreply.github.com>
2024-09-24 09:38:10 +0300

    feat: add response shemas and constructors

---
+++ src/fastapi_scaffold/responses.py
@@ -0,0 +1,56 @@
+from typing import Self, Sequence
+
+from pydantic import BaseModel, ConfigDict, create_model
+
+from .pagination import PaginationParams, PaginationSchema
+
+
+class Schema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BaseResponse(Schema):
+    success: bool = True
+    message: str = ""
+
+
+class DataResponse[Data](BaseResponse):
+    success: bool = True
+    message: str = "Data retrieved successfully"
+    data: Data
+
+    @classmethod
+    def single_by_key(cls, key: str, schema: BaseModel) -> Self:
+        data_model = create_model(schema.__name__, **{key: (schema, ...)})
+        return cls[data_model]
+
+
+class ListData[ListItem](BaseModel):
+    list: Sequence[ListItem]
+
+
+class ListResponse[ListItem](DataResponse[ListItem]):
+    data: ListData[ListItem]
+    pagination: PaginationSchema
+
+    @staticmethod
+    def _get_list_elements_type[T](items: list[T]) -> type[T] | None:
+        for item in items:
+            return type(item)
+
+    @classmethod
+    def from_list(
+        cls,
+        items: Sequence[ListItem],
+        total_count: int,
+        params: PaginationParams,
+        response_message: str | None,
+    ) -> Self:
+        message_kwarg = {}
+        if response_message is not None:
+            message_kwarg = {"message": response_message}
+        return cls(
+            data=ListData[cls._get_list_elements_type(items)](list=items),
+            pagination=PaginationSchema.from_params(params, total_count),
+            **message_kwarg,
+        )
